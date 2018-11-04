28d9b2e740302fe11d4655e09cc14a852e0cead1
Kolyamba <haravetskyy@gmail.com>
2018-11-05 00:02:59 +0100

    messages and users data are now sent properly minor refactor

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,58 @@
-const express = require('express');
-const socketIO = require('socket.io');
-import * as http from 'http';
+import express, {Request, Response} from 'express';
+import socketIO, {Socket} from 'socket.io';
+import {Server} from 'http';
 import * as _ from 'lodash';
 import * as validUrl from 'valid-url';
 
-import {User} from "./models/user.model";
-import {Message} from "./models/message.model";
+import {User} from './models/user.model';
+import {Message} from './models/message.model';
 import {parse} from './link-parser/link-parser';
 
-const app = express();
-const server = new http.Server(app);
-const io = socketIO(server);
+const app: express.Application = express();
+const server: Server = new Server(app);
+const io: socketIO.Server = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 let users: User[] = [];
 let messages: Message[] = [];
 
-io.on('connect', (socket: any) => {
-  console.log('New client connected...');
+app.get('/refresh/users', (req: Request, res: Response) => {
+  res.send(users);
+});
 
-  socket.emit('users', users);
-  socket.emit('messages', messages);
+app.get('/refresh/messages', (req: Request, res: Response) => {
+  res.send(messages);
+});
 
-  socket.on('user', (user: User) => {
-    user.id = socket.id;
-    io.emit('user', user);
-    console.log('New user joined: ', user);
+io.on('connect', (socket: Socket) => {
+    console.log('New client connected...');
 
-    users.push(user);
-    console.log('Users: ', users);
-  });
+    socket.on('user', (user: User) => {
+        user.id = socket.id;
+        io.emit('user', user);
+        console.log('New user joined: ', user);
 
-  socket.on('message', async (message: Message) => {
-    message.id = _.uniqueId();
+        users.push(user);
+        console.log('Users: ', users);
+    });
 
-    if (validUrl.isUri(message.text)) {
-      message.link = await parse(message.text);
-    }
-    message.at = new Date().toLocaleTimeString();
+    socket.on('message', async (message: Message) => {
+        message.id = _.uniqueId();
 
-    io.emit('message', message);
-    console.log('New message received: ', message);
+        if (validUrl.isUri(message.text)) {
+            message.link = await parse(message.text);
+        }
+        message.at = new Date().toLocaleTimeString();
 
-    messages.push(message);
-    console.log('Messages: ', messages);
-  });
+        io.emit('message', message);
+        console.log('New message received: ', message);
 
-  socket.on('disconnect', () => console.log('Client disconnected...'));
+        messages.push(message);
+        console.log('Messages: ', messages);
+    });
+
+    socket.on('disconnect', () => console.log('Client disconnected...'));
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}...`));
--- src/link-parser/link-parser.ts
+++ src/link-parser/link-parser.ts
@@ -1,8 +1,7 @@
+const ogs = require('open-graph-scraper');
 import {Link} from "../models/link.model";
 
-const ogs = require('open-graph-scraper');
-
-export function parse(url: string) {
+export function parse(url: string): Promise<Link> {
     return ogs({'url': url})
         .then((result: any) => {
             return new Link(result.data.ogUrl, result.data.ogTitle, result.data.ogDescription, result.data.ogImage.url);
